de3137aa0c1f06a165eaf351f0f9ffcd6ab7dfb1
tangjiaolin <64716061+tangjiaolin@users.noreply.github.com>
2024-01-30 14:31:03 +0800

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+import sys
+
+def printOperator(operatorName):
+    print(f'operation name:, {operatorName}')
+
+
+if __name__ = "__main__":
+    operatorName = sys.argv[1]
+    printOperator(operatorName)
