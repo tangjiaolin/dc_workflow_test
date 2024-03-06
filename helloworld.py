1e60a081e31e58d035b79e91a37a2e8eef4432ad
jilian.tang <jilian.tang@veoneer.com>
2024-03-06 14:21:42 +0800

    delete

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def printOperator(operatorName):
-    print(f'operation name:{operatorName}')
-
-
-if __name__ == "__main__":
-    operatorName = sys.argv[1]
-    printOperator(operatorName)
